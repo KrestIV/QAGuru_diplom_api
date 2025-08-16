1b3f378de6cf875b02beedae86bc8f320d3b667f
Ilya Krestsov <krestsov.iv@yandex.ru>
2025-08-16 23:58:26 +0200

    faker overhaul (bug fix)

--- src/test/java/models/PetModel.java
+++ src/test/java/models/PetModel.java
@@ -22,21 +22,18 @@ public class PetModel {
         this.name = faker.dog().name();
 
         int k = faker.number().numberBetween(0,5);
-        String[] urlArray = new String[k];
+        this.photoUrls = new String[k];
         for(int i = 0; i < k; i++)
         {
-            urlArray[i] = faker.internet().url();
+            this.photoUrls[i] = faker.internet().url();
         }
 
         k = faker.number().numberBetween(0,5);
-        PetTagModel[] tagArray = new PetTagModel[k];
+        this.tags = new PetTagModel[k];
         for(int i = 0; i < k; i++)
         {
-            tagArray[i] = new PetTagModel(faker.number().numberBetween(0L, Long.MAX_VALUE), faker.color().name());
+            this.tags[i] = new PetTagModel(faker.number().numberBetween(0L, Long.MAX_VALUE), faker.color().name());
         }
-
-//        this.photoUrls = new String[]{faker.internet().url(), faker.internet().url()};
-//        this.tags = new PetTagModel[]{new PetTagModel(faker.number().numberBetween(0L, Long.MAX_VALUE), faker.color().name())};
 
         this.status = faker.options().option(PetStatus.class).getStatus();
     }
